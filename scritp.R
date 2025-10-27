66ed016b909a6a58f51fd33996c90fd24482f61f
Gisela Trejo <322033834@iztacala.unam.mx>
2025-10-26 19:25:26 -0600

    Add files via upload

---
+++ scritp.R
@@ -0,0 +1,19 @@
+#Titulo: Trabajo final de interacciones a.k.a maripositas
+#Autores: Trejo Zarco GIisela , Ailyyn , Mafer y Luis
+#Fecha: 24/10/25 - 
+#Objetivos: 
+#Notas: No mueras computadora , amén
+#------------
+#Paquetes 
+library(ggplot2)
+library(bipartite)
+library(readxl)
+library(dplyr)
+library(viridis)
+library(readr)
+#---------------- 
+
+
+Base2 <- read.csv("mariposita_3.csv",row.names = 1)
+base2 <-  read_xlsx("mariposita2.1.xlsx")
+read
